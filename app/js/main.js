fd4d74f0a822cc510087944332d15984bdacd66f
mahmud-makhsudov <78651721+mahmud-makhsudov@users.noreply.github.com>
2021-08-27 13:51:15 +0500

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -5,3 +5,11 @@ const swiper = new Swiper('.slider', {
     prevEl: '.swiper-button-prev',
   }
 });
+
+const headerBtn = document.querySelector('.header__dropdown')
+const headerList = document.querySelector('.header__menu-list')
+
+headerBtn.addEventListener('click', () => {
+  headerBtn.classList.toggle('active')
+  headerList.classList.toggle('active')
+})
