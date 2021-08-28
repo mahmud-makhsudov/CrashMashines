c738c027b4d8647b8963a9c10ac3b7d36fa122b8
mahmud-makhsudov <78651721+mahmud-makhsudov@users.noreply.github.com>
2021-08-28 15:18:06 +0500

    fiz

--- app/js/main.js
+++ app/js/main.js
@@ -13,26 +13,3 @@ headerBtn.addEventListener("click", () => {
   headerBtn.classList.toggle("active");
   headerList.classList.toggle("active");
 });
-
-const smoothLinks = document.querySelectorAll('a[href="#form"]');
-for (let smoothLink of smoothLinks) {
-  smoothLink.addEventListener("click", function (e) {
-    e.preventDefault();
-    const id = smoothLink.getAttribute("href");
-
-    document.querySelector(id).scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  });
-}
-
-window.onload = function () {
-  var cssLink = document.createElement("link");
-  cssLink.href = "css/style.min.css";
-  cssLink.rel = "stylesheet";
-  cssLink.type = "text/css";
-  document.getElementById("iframe").contentDocument.head.appendChild(cssLink);
-
-  console.log(document.getElementById("iframe").contentDocument);
-};
